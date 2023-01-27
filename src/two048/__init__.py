5d7c32c9f88f9dc5648886d6a1c47fcde9d98e89
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2023-01-27 22:21:40 +0100

    transform program as a package

---
+++ src/two048/__init__.py
@@ -0,0 +1,11 @@
+__title__ = "two048"
+__author__ = "airo.pi_"
+__license__ = "MIT"
+__version__ = "0.1"
+
+from .main import Direction as Direction
+from .main import Movement as Movement
+from .main import Tile as Tile
+from .main import Two048 as Two048
+
+__all__ = ["Direction", "Movement", "Two048", "Tile"]
